5927d092be6d4ea9cd9abd2c644457582a186508
“Green <greenojegwo@gmail.com>
2022-09-12 20:16:55 +0100

    first Amazon clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Home from './Home';
 
 function App() {
   return (
+    //BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello Guy/s, Green here, let's build Amazon Clone </h1> */}
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxBNaDT4dEc4Be1yvNcoSc8EBZexG_kfQ",
+  authDomain: "challenge-d5898.firebaseapp.com",
+  projectId: "challenge-d5898",
+  storageBucket: "challenge-d5898.appspot.com",
+  messagingSenderId: "769032048730",
+  appId: "1:769032048730:web:29c0073dc57a113a370863",
+  measurementId: "G-1VCET5FE4P"
+};
