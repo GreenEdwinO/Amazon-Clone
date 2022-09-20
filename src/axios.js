e02ed6a0caed70758299c3364d88140733c99d4d
“Green <greenojegwo@gmail.com>
2022-09-20 15:15:28 +0100

    Firebased Backend Live plus Order/s Page

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   //THE API CLOUD FUNCTION URL
-  baseURL: 'http://localhost:5001/challenge-d5898/us-central1/api'
+  baseURL: 'https://us-central1-challenge-d5898.cloudfunctions.net/api'
 
 });
 
